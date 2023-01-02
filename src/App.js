1952a27a2a32bf2a4ffc2fc8ac93a666a2976d33
Chrsteffen <Chrsteffen1@gmail.com>
2023-01-02 13:12:07 -0500

    make form component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import Paper from '@mui/material/Paper'
-import TextField from '@mui/material/TextField'
-import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem'
-import ListItemText from '@mui/material/ListItemText'
-import Divider from '@mui/material/Divider'
-import PersonIcon from '@mui/icons-material/Person'
-import LocationOnIcon from '@mui/icons-material/LocationOn'
-import EmailIcon from '@mui/icons-material/Email'
-import ListItemIcon from '@mui/material/ListItemIcon'
 import Form from './Components/Form';
 import Display from './Components/Display';
 
@@ -45,63 +33,7 @@ function App() {
   return (
     <div className="App">
       <Display display={display}/>
-      <Box 
-      display="flex"
-      justifyContent='center'
-      alignItems='center'
-      sx={{width:'50%', mt: '2rem'}}>
-        <Paper elevation={4} sx={{ width: "100%", p: "1rem" }}>
-          <form onSubmit={handleSubmit}>
-            <TextField
-                type="text"
-                name="firstName"
-                label="First Name"
-                value={formData.firstName} 
-                onChange={handleChange}
-                autoComplete="off"
-                margin="normal"
-                fullWidth
-                required
-            />
-            <TextField
-                type="text"
-                name="lastName"
-                label="Last Name"
-                value={formData.lastName}
-                onChange={handleChange}
-                autoComplete="off"
-                margin="normal"
-                fullWidth
-                required
-            />
-            <TextField
-                type="text"
-                name="email"
-                label="Email"
-                value={formData.email}
-                onChange={handleChange}
-                autoComplete="off"
-                margin="normal"
-                fullWidth
-                required
-                
-            />
-            <TextField
-                type="text"
-                name="location"
-                label="Location"
-                value={formData.location}
-                onChange={handleChange}
-                autoComplete="off"
-                margin="normal"
-                fullWidth
-                required
-                
-            />
-            <Button type="submit" variant='contained' fullWidth color='secondary'> Submit </Button>
-          </form>
-        </Paper>
-      </Box>
+      <Form handleChange={handleChange} handleSubmit={handleSubmit} formData={formData}/>
     </div>
   );
 }
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,7 +1,67 @@
-const Form = () => {
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Paper from '@mui/material/Paper'
+import TextField from '@mui/material/TextField'
+
+const Form = ({handleChange, handleSubmit, formData}) => {
   return (  
-    <>
-    </>
+    <Box 
+    display="flex"
+    justifyContent='center'
+    alignItems='center'
+    sx={{width:'50%', mt: '2rem'}}>
+      <Paper elevation={4} sx={{ width: "100%", p: "1rem" }}>
+        <form onSubmit={handleSubmit}>
+          <TextField
+              type="text"
+              name="firstName"
+              label="First Name"
+              value={formData.firstName} 
+              onChange={handleChange}
+              autoComplete="off"
+              margin="normal"
+              fullWidth
+              required
+          />
+          <TextField
+              type="text"
+              name="lastName"
+              label="Last Name"
+              value={formData.lastName}
+              onChange={handleChange}
+              autoComplete="off"
+              margin="normal"
+              fullWidth
+              required
+          />
+          <TextField
+              type="text"
+              name="email"
+              label="Email"
+              value={formData.email}
+              onChange={handleChange}
+              autoComplete="off"
+              margin="normal"
+              fullWidth
+              required
+              
+          />
+          <TextField
+              type="text"
+              name="location"
+              label="Location"
+              value={formData.location}
+              onChange={handleChange}
+              autoComplete="off"
+              margin="normal"
+              fullWidth
+              required
+              
+          />
+          <Button type="submit" variant='contained' fullWidth color='secondary'> Submit </Button>
+        </form>
+      </Paper>
+    </Box>
   );
 }
 
