b1fa869d93231a835356fbe435f3c676e6d08c41
Chrsteffen <Chrsteffen1@gmail.com>
2023-01-02 11:08:10 -0500

    create form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 import { useState } from 'react';
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Paper from '@mui/material/Paper'
+import TextField from '@mui/material/TextField'
 
 function App() {
   const [display, setDisplay] = useState({
@@ -17,6 +21,37 @@ function App() {
 
   return (
     <div className="App">
+      <Box>
+        <Paper>
+          <form>
+            <TextField
+              type="text"
+              name="firstName"
+              label="First Name"
+              value={formData.firstName}
+            />
+            <TextField
+              type="text"
+              name="lastName"
+              label="Last Name"
+              value={formData.lastName}
+            />
+            <TextField
+              type="text"
+              name="email"
+              label="Email"
+              value={formData.email}
+            />
+            <TextField
+              type="text"
+              name="location"
+              label="Location"
+              value={formData.location}
+            />
+            <Button type='submit'>Submit</Button>
+          </form>
+        </Paper>
+      </Box>
     </div>
   );
 }
